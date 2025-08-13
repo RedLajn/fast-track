57ba74b4a6575418bfa6fd0420c9a6cc6b042c66
RedLajn <mateuszwojciechowski1620@gmail.com>
2025-08-13 17:49:18 +0200

    Configure database sessions

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -10,17 +14,24 @@ final class ConferenceController extends AbstractController
 {
     ///#[Route('/conference', name: 'app_conference')]
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(ConferenceRepository $conferenceRepository): Response
     {
-        return new Response (<<<EOF
-        <html>
-            <head>  
-                <body>
-                    <img src="/images/under-construction.gif" />
-                </body>
-            </head>
-        </html>
-        EOF);
+        return $this->render('conference/index.html.twig', [
+                        'conferences' => $conferenceRepository->findAll(),
+                    ]);
+    }
 
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::COMMENTS_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::COMMENTS_PER_PAGE),
+        ]);
     }
 }
